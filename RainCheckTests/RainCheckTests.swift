0198b5c4433affd0b33ce28f6eef85c40970d267
Matt McDonnell <mattmcdonnell02@gmail.com>
2025-01-17 23:14:13 -0600

    Initial Commit

---
+++ RainCheckTests/RainCheckTests.swift
@@ -0,0 +1,17 @@
+//
+//  RainCheckTests.swift
+//  RainCheckTests
+//
+//  Created by Matt McDonnell on 1/17/25.
+//
+
+import Testing
+@testable import RainCheck
+
+struct RainCheckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
